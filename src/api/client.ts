ce7081a749b5b599a2a2a44d0936a3aa5562c6ab
Radymyr <loboda.radymyr@gmail.com>
2024-06-05 18:28:37 +0300

    added header

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    'Content-Type': 'multipart/form-data',
-    Accept: 'application/json',
-  },
 });
