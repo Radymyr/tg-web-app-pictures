d656e27c23478615c10a6e80997459cf5e72a47a
Radymyr <loboda.radymyr@gmail.com>
2024-08-05 01:58:26 +0300

    added photo types

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -12,6 +12,7 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({ onChange }) => {
         accept="image/png, image/jpeg"
         type="file"
         onChange={onChange}
+        capture="environment"
       />
     </label>
   );
